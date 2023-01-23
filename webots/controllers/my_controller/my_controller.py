755a092cf717936e0233684be5eec364679f195a
amortea1 <ahmed.alsaiedi@hotmail.com>
2023-01-23 10:45:09 +0100

    a3

--- webots/controllers/my_controller/my_controller.py
+++ webots/controllers/my_controller/my_controller.py
@@ -19,6 +19,7 @@ robot = Robot()
 # get the time step of the current world.
 #timestep = int(robot.getBasicTimeStep())
 timestep = 64
+max_speed=65
 
 
 Front_left_wheel = robot.getDevice('front right wheel motor')
@@ -38,21 +39,59 @@ Rear_left_wheel.setVelocity(0.0)
 Rear_right_wheel.setPosition(float('inf'))
 Rear_right_wheel.setVelocity(0.0)
 
+
+def Front():
+    Front_left_speed= 1.5*max_speed
+    Front_right_speed = 1.5*max_speed
+    Rear_left_speed = 1.5*max_speed
+    Rear_right_speed = 1.5*max_speed
+
+
+    Front_left_wheel.setVelocity(Front_left_speed)
+    Front_right_wheel.setVelocity(Front_right_speed)
+    Rear_left_wheel.setVelocity(Rear_left_speed)
+    Rear_right_wheel.setVelocity(Rear_right_speed)
+
+def Right():
+    Front_left_speed= 1.5*max_speed
+    Front_right_speed = -1.5*max_speed
+    Rear_left_speed = -1.5*max_speed
+    Rear_right_speed = 1.5*max_speed
+
+
+    Front_left_wheel.setVelocity(Front_left_speed)
+    Front_right_wheel.setVelocity(Front_right_speed)
+    Rear_left_wheel.setVelocity(Rear_left_speed)
+    Rear_right_wheel.setVelocity(Rear_right_speed)
+
+def Left():
+    Front_left_speed= -1.5*max_speed
+    Front_right_speed = 1.5*max_speed
+    Rear_left_speed = 1.5*max_speed
+    Rear_right_speed =-1.5*max_speed
+
+
+    Front_left_wheel.setVelocity(Front_left_speed)
+    Front_right_wheel.setVelocity(Front_right_speed)
+    Rear_left_wheel.setVelocity(Rear_left_speed)
+    Rear_right_wheel.setVelocity(Rear_right_speed)
+
+
 # You should insert a getDevice-like function in order to get the
 # instance of a device of the robot. Something like:
 #  motor = robot.getDevice('motorname')
 #  ds = robot.getDevice('dsname')
 #  ds.enable(timestep)
 
+
+
 # Main loop:
 # - perform simulation steps until Webots is stopping the controller
 while robot.step(timestep) != -1:
 
-    #left_speed= 0.5*max_speed
-    #right_speed = 0.5*max_speed
+    
 
-    #left_motor.setVelocity(left_speed)
-    #right_motor.setVelocity(left_speed)
+    Right()
 
     # Read the sensors:
     # Enter here functions to read sensor data, like:
