a038281ff206bb601e296cba4443658be2c36994
amortea1 <ahmed.alsaiedi@hotmail.com>
2023-01-23 09:26:09 +0100

    a

--- webots/controllers/my_controller/my_controller.py
+++ webots/controllers/my_controller/my_controller.py
@@ -10,14 +10,33 @@
 
 # You may need to import some classes of the controller module. Ex:
 #  from controller import Robot, Motor, DistanceSensor
-from controller import Robot
+from controller import Robot, Motor, PositionSensor
 #doit ajouter l'option Robot souligner
 
 # create the Robot instance.
 robot = Robot()
 
 # get the time step of the current world.
-timestep = int(robot.getBasicTimeStep())
+#timestep = int(robot.getBasicTimeStep())
+timestep = 64
+
+
+Front_left_wheel = robot.getMotor('front right wheel motor')
+Front_right_wheel= robot.getMotor('front left wheel motor')
+Rear_left_wheel = robot.getMotor('rear left wheel motor')
+Rear_right_wheel = robot.getMotor('rear right wheel motor')
+
+Front_left_wheel.setPosition(float('inf'))
+Front_left_wheel.setVelocity(0.0)
+
+Front_right_wheel.setPosition(float('inf'))
+Front_right_wheel.setVelocity(0.0)
+
+Rear_left_wheel.setPosition(float('inf'))
+Rear_left_wheel.setVelocity(0.0)
+
+Rear_right_wheel.setPosition(float('inf'))
+Rear_right_wheel.setVelocity(0.0)
 
 # You should insert a getDevice-like function in order to get the
 # instance of a device of the robot. Something like:
@@ -28,6 +47,13 @@ timestep = int(robot.getBasicTimeStep())
 # Main loop:
 # - perform simulation steps until Webots is stopping the controller
 while robot.step(timestep) != -1:
+
+    #left_speed= 0.5*max_speed
+    #right_speed = 0.5*max_speed
+
+    #left_motor.setVelocity(left_speed)
+    #right_motor.setVelocity(left_speed)
+
     # Read the sensors:
     # Enter here functions to read sensor data, like:
     #  val = ds.getValue()
